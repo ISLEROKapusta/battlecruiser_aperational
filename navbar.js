376108fdbc6e1004ba872dedf2a8b365dd0a1ecb
EdelweissPC <EdelweissPC@DESKTOP-8KQ759Q>
2023-12-08 21:36:53 +0300

    Добавьте файлы проекта.

---
+++ navbar.js
@@ -0,0 +1,26 @@
+﻿var prevScrollpos = window.pageYOffset;
+var isMouseOverNavbar = false;
+
+function showNavbar() {
+    isMouseOverNavbar = true;
+    document.getElementById("navbar").style.top = "0";
+}
+
+function hideNavbar() {
+    isMouseOverNavbar = false;
+    if (prevScrollpos === window.pageYOffset) {
+        document.getElementById("navbar").style.top = "-50px"; // Adjust the hiding distance as needed!!!
+    }
+}
+
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (!isMouseOverNavbar) {
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-50px"; // Adjust the hiding distance as needed!!!
+        }
+    }
+    prevScrollpos = currentScrollPos;
+}
